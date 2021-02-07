5873ab87db80d3ac0a45a5aa3c4021d3d2936d0f
saoki <saoki@student.42tokyo.jp>
2021-02-07 12:40:51 +0900

    c

--- get_next_line.c
+++ get_next_line.c
@@ -116,6 +116,7 @@ int				ft_rewrite_line(char *temp, char **remainder, char **line)
 	return (0);
 }
 
+/*
 char		*ft_gnl_strjoin(char *s1, char *s2)
 {
 	char	*str;
@@ -142,5 +143,35 @@ char		*ft_gnl_strjoin(char *s1, char *s2)
 	str[i] = '\0';
 	free(s1);
 	s1 = NULL;
+	return (str);
+}
+*/
+
+
+char	*ft_gnl_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
 	return (str);
 }
